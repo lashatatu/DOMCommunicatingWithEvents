d7010efee728af38aade1bda860f4ba906287211
lashatatu <lashatatu@gmail.com>
2020-09-22 17:40:08 +0400

    1

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const btn = document.querySelector('button')
 
 btn.addEventListener("mouseover", function () {
     console.log("mouse over");
-    btn.style.left = '200px';
-    btn.style.top = '100px';
+    const height = Math.floor(Math.random() * window.innerHeight);
+    const width = Math.floor(Math.random() * window.innerWidth);
+    btn.style.left = `${width}px`;
+    btn.style.top = `${height}px`;
 });
+
+btn.addEventListener(`click`, function () {
+    btn.innerText = `you got me`;
+    document.body.style.backgroundColor = `green`;
+})
