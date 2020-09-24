3552054542733e1b4a4ef2bd0fd997dd7d5e79e9
lashatatu <lashatatu@gmail.com>
2020-09-24 11:04:26 +0400

    1

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const btn = document.querySelector('button')
+const colors = [`red`, `orange`, `yellow`, `green`, `blue`, `purple`, `indigo`, `violet`];
 
-btn.addEventListener("mouseover", function () {
-    console.log("mouse over");
-    const height = Math.floor(Math.random() * window.innerHeight);
-    const width = Math.floor(Math.random() * window.innerWidth);
-    btn.style.left = `${width}px`;
-    btn.style.top = `${height}px`;
-});
 
-btn.addEventListener(`click`, function () {
-    btn.innerText = `you got me`;
-    document.body.style.backgroundColor = `green`;
-})
+const printColor = function (box) {
+    console.log(this.style.backgroundColor);
+}
+const container = document.querySelector(`#boxes`)
+for (let color of colors) {
+    const box = document.createElement(`div`);
+    box.style.backgroundColor = color;
+    box.classList.add(`box`);
+    container.appendChild(box);
+    box.addEventListener(`click`, printColor)
+}
